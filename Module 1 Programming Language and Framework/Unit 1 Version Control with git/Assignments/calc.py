b203f348b75c2e36b46a30fa6b487a6de25c755e
pawan <pawan@fusemachines.com>
2023-08-02 21:46:48 +0545

    Added a Arithmetic Calculator
    Program to calculate the Addition , Muliplcation and Subtraction

---
+++ Module 1 Programming Language and Framework/Unit 1 Version Control with git/Assignments/calc.py
@@ -0,0 +1,39 @@
+def addition(a:float , b: float ): 
+    """Adds two number
+
+    Args:
+        a (float): first number  
+        b (float): second number
+
+    Returns:
+        float: sum of a and db 
+    """
+    return a + b 
+
+
+def subtract(a:float , b: float ): 
+    """subtracts two numbers 
+
+    Args:
+        a (float): first number 
+        b (float): second number 
+
+    Returns:
+        float: subtraction of b from a 
+    """
+    return a - b 
+
+
+
+def multiply(a:float , b: float ): 
+    """MUltiplies two number 
+
+    Args:
+        a (float): first number 
+        b (float): second number 
+
+    Returns:
+        float: multiplication of two numbbers 
+    """
+    return a * b 
+
