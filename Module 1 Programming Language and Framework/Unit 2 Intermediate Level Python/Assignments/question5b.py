1cc0ed779b8d9cf8cd77c871a87b9e31b372a5dc
pawan <pawan@fusemachines.com>
2023-08-03 22:51:09 +0545

    completed

---
+++ Module 1 Programming Language and Framework/Unit 2 Intermediate Level Python/Assignments/question5b.py
@@ -0,0 +1,15 @@
+'''
+Given two lists of integers, create a list that contains the
+product of each element of the first list with the corresponding element in the
+second list using list comprehension.
+'''
+
+
+list1 = [1,5,6,7,8]
+list2 = [5,6,7,4,2]
+
+
+
+# can we chain the two comprehnsion in 1 lets try 
+result = [x*y for x,y in zip(list1, list2)]
+print(result)
