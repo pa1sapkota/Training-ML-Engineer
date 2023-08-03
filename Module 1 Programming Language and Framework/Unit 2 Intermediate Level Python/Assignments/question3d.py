53a094925fbff697fd6a3cd04dc7bc1c5e8c7117
pawan <pawan@fusemachines.com>
2023-08-03 22:36:16 +0545

    Not Completed

---
+++ Module 1 Programming Language and Framework/Unit 2 Intermediate Level Python/Assignments/question3d.py
@@ -0,0 +1,9 @@
+'''
+Implement a function called check_prime that takes a positive integer as input and
+uses a ternary operator to determine if it's a prime number. Return "Prime" if it is,
+otherwise "Not Prime."
+'''
+
+
+def check_prime(): 
+    pass 
