a931a091da5031b833120ae13a23694328745c94
pa1sapkota <141208184+pa1sapkota@users.noreply.github.com>
2023-08-08 14:12:21 +0545

    Delete Module 1 Programming Language and Framework/Unit 5 RestAPI/Assignment/app directory

--- Module 1 Programming Language and Framework/Unit 5 RestAPI/Assignment/app/database.py
+++
@@ -1,6 +0,0 @@
-import sqlite3 
-
-# create a connection to the database 
-conn = sqlite3.connect('data.db') 
-cursor = conn.cursor() 
-
--- Module 1 Programming Language and Framework/Unit 5 RestAPI/Assignment/app/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class EmployeeCreate(BaseModel):
-    name: str
-    department: str
-
-class Employee(EmployeeCreate):
-    id: int
