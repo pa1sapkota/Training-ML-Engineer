c6b66a9dd89cd81ff8b39364a7cda207eb36b01c
pawan <pawan@fusemachines.com>
2023-08-08 13:52:04 +0545

    new

--- Module 1 Programming Language and Framework/Unit 5 RestAPI/Assignment/app/main.py
+++ Module 1 Programming Language and Framework/Unit 5 RestAPI/Assignment/app/main.py
@@ -22,39 +22,3 @@ app = FastAPI()
 @app.put("/employees/{employee_id}/{column}/{new_value}")
 # update the table using employeeid and set new value of a column
 
-
-
-
-
-
-
-@app.put("/employees/{employee_id}", response_model=Employee)
-async def update_employee(employee_id: int, employee_update: EmployeeCreate):
-    cursor.execute("SELECT id FROM employee WHERE id=?", (employee_id,))
-    existing_employee = cursor.fetchone()
-    if not existing_employee:
-        raise HTTPException(status_code=404, detail="Employee not found")
-
-    cursor.execute(
-        "UPDATE employee SET name=?, department=? WHERE id=?",
-        (employee_update.name, employee_update.department, employee_id),
-    )
-    conn.commit()
-    return {"id": employee_id, **employee_update.dict()}
-
-@app.delete("/employees/{employee_id}")
-async def delete_employee(employee_id: int):
-    cursor.execute("SELECT id FROM employee WHERE id=?", (employee_id,))
-    existing_employee = cursor.fetchone()
-    if not existing_employee:
-        raise HTTPException(status_code=404, detail="Employee not found")
-
-    cursor.execute("DELETE FROM employee WHERE id=?", (employee_id,))
-    conn.commit()
-    return {"message": "Employee deleted successfully"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
