7a331c9bdb992b9e15f96c0b3a3f334c9a52b21c
pawan <pawan@fusemachines.com>
2023-08-08 13:29:09 +0545

    Remaining to implement Composition

--- Module 1 Programming Language and Framework/Unit 2 Intermediate Level Python/Assignments/question8a.py
+++ Module 1 Programming Language and Framework/Unit 2 Intermediate Level Python/Assignments/question8a.py
@@ -1,20 +1,45 @@
-from typing import List
-
-
-
-
+'''
+Create a Python class to represent a University. The university should have
+attributes like name, location, and a list of departments. Implement encapsulation to
+protect the internal data of the University class. Create a Department class that
+inherits from the University class. The Department class should have attributes like
+department name, head of the department, and a list of courses offered. Implement
+polymorphism by defining a common method for both the University and
+Department classes to display their details.
+'''
 class University: 
-    def __init__(self, name): 
+    def __init__(self, name, location = None): 
         self.__name = name 
+        self.__location = location
+        self.__departments= []  
 
+    def add_department(self, department):
+        self.__departments.append(department)
 
+    def display_details(self):
+        print(f"University: {self.__name}")
+        print(f"Location: {self.__location}")
+        print("Departments:")
+        for department in self.__departments:
+            print("Print the Department Details ")
 class Department(University): 
-    def __init__(self, name , hod, course_offered): 
+    def __init__(self, name , hod, courses): 
         super.__init__(name) 
         self.__hod = hod 
-        self.__course_offered = course_offered
+        self.__courses = courses
+    def add_course(self, course):
+        self.__courses.append(course)
+
+    def display_details(self):
+        print(f"Department: {self.get_name()}")
+        print(f"Head of Department: {self.__hod}")
+        print("Courses offered:")
+        for course in self.__courses:
+            print(f"- {course}")
 
 
+university = University("Tribhuvan University", "Lalitpur")
+university.display_details()
 
 
 
